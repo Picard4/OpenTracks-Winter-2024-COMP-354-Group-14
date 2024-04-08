e90c3a8d1bb8db5431c1d489133869c90b567a91
Marmik patel <62463824+marmik28@users.noreply.github.com>
2024-04-08 19:19:31 -0400

    Added feature that sorts the rankings based on the distance and moving time.

--- src/main/java/de/dennisguse/opentracks/ui/leaderboard/leaderboardFragment/DistanceLeaderboardFragment.java
+++ src/main/java/de/dennisguse/opentracks/ui/leaderboard/leaderboardFragment/DistanceLeaderboardFragment.java
@@ -1,82 +1,134 @@
 package de.dennisguse.opentracks.ui.leaderboard.leaderboardFragment;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import de.dennisguse.opentracks.data.models.Ranking;
+import de.dennisguse.opentracks.data.models.Distance;
 import de.dennisguse.opentracks.ui.leaderboard.LeaderboardPagerAdapter;
 
 public class DistanceLeaderboardFragment extends LeaderboardFragment {
-    private boolean averageRefresh;
-    private boolean bestRefresh;
 
     @Override
     protected List<Ranking> calculateLatestAverageRankingsData(List<LeaderboardPagerAdapter.PlaceHolderTrackUser> latestLeaderboardData) {
-        // TODO: Replace the test data with code that gathers the appropriate Ranking data
-        List<Ranking> latestRankingsData;
-        if (!averageRefresh)
-            // Get a different data set if this is the first time the rankings data is being collected
-            latestRankingsData = getTestData();
-        else
-            latestRankingsData = getAltTestData();
-        // All future rankings data collections should be refreshes
-        averageRefresh = true;
-        sortRankingsByDistance(latestRankingsData);
-        return latestRankingsData;
+        Map<String, SummedStatTrackUser> statsMap = new HashMap<>();
+        for (LeaderboardPagerAdapter.PlaceHolderTrackUser trackUser : latestLeaderboardData) {
+            if (!trackUser.socialAllow)
+                continue;
+
+            if (!statsMap.containsKey(trackUser.nickname)) {
+                statsMap.put(trackUser.nickname, new SummedStatTrackUser(trackUser));
+            }
+            else {
+                SummedStatTrackUser existingRecord = statsMap.get(trackUser.nickname);
+                existingRecord.getPlaceHolderTrackUser().trackStatistics.setTotalDistance(
+                        new Distance(existingRecord.getPlaceHolderTrackUser().trackStatistics.getTotalDistance().distance_m()
+                                + trackUser.trackStatistics.getTotalDistance().distance_m())
+                );
+                existingRecord.incrementSumFactorCount();
+            }
+        }
+        List<SummedStatTrackUser> latestSummedLeaderboardData = new ArrayList<>(statsMap.values());
+        latestSummedLeaderboardData.sort(new SortByAverageDistance());
+
+        List<Ranking> rankingsData = new ArrayList<>();
+        int rank = 0;
+        int consecutiveTies = 0;
+        String lastScore = "";
+        for (SummedStatTrackUser summedStatTrackUser : latestSummedLeaderboardData) {
+            Ranking nextRanking = new Ranking(
+                    ++rank,
+                    summedStatTrackUser.getPlaceHolderTrackUser().nickname,
+                    summedStatTrackUser.getPlaceHolderTrackUser().location,
+                    getDistanceDisplay(getAverageDistanceFromSummedStatTrackUser(summedStatTrackUser))
+            );
+            if (nextRanking.getScore().equals(lastScore)) {
+                consecutiveTies++;
+                nextRanking.setRank(rank - consecutiveTies);
+            }
+            else
+                consecutiveTies = 0;
+            rankingsData.add(nextRanking);
+            lastScore = nextRanking.getScore();
+        }
+        return rankingsData;
+    }
+
+    private class SortByAverageDistance implements Comparator<SummedStatTrackUser> {
+        @Override
+        public int compare(SummedStatTrackUser user1, SummedStatTrackUser user2) {
+            Distance user1AverageDistance = getAverageDistanceFromSummedStatTrackUser(user1);
+            Distance user2AverageDistance = getAverageDistanceFromSummedStatTrackUser(user2);
+
+            if (user1AverageDistance.lessThan(user2AverageDistance))
+                return 1;
+            else if (user1AverageDistance.greaterThan(user2AverageDistance))
+                return -1;
+            return 0;
+        }
+    }
+
+    private String getDistanceDisplay(Distance distance) {
+        return distance.distance_m() + " m";
+    }
+
+    private Distance getAverageDistanceFromSummedStatTrackUser(SummedStatTrackUser summedStatTrackUser) {
+        return new Distance(summedStatTrackUser.getPlaceHolderTrackUser().trackStatistics.getTotalDistance().distance_m() / summedStatTrackUser.getSumFactorCount());
     }
 
     @Override
     protected List<Ranking> calculateLatestBestRankingsData(List<LeaderboardPagerAdapter.PlaceHolderTrackUser> latestLeaderboardData) {
-        // TODO: Replace the test data with code that gathers the appropriate Ranking data
-        List<Ranking> latestRankingsData;
-        if (!bestRefresh)
-            latestRankingsData = getAltTestData();
-        else
-            latestRankingsData = getTestData();
-        bestRefresh = true;
-        sortRankingsByDistance(latestRankingsData);
-        return latestRankingsData;
+        Map<String, LeaderboardPagerAdapter.PlaceHolderTrackUser> statsMap = new HashMap<>();
+        for (LeaderboardPagerAdapter.PlaceHolderTrackUser trackUser : latestLeaderboardData) {
+            if (!trackUser.socialAllow)
+                continue;
+
+            if (!statsMap.containsKey(trackUser.nickname) ||
+                    statsMap.get(trackUser.nickname).trackStatistics.getTotalDistance().lessThan(trackUser.trackStatistics.getTotalDistance())) {
+                statsMap.put(trackUser.nickname, trackUser);
+            }
+        }
+        latestLeaderboardData = new ArrayList<>(statsMap.values());
+        latestLeaderboardData.sort(new SortByBestDistance());
+
+        List<Ranking> rankingsData = new ArrayList<>();
+        int rank = 0;
+        int consecutiveTies = 0;
+        String lastScore = "";
+        for (LeaderboardPagerAdapter.PlaceHolderTrackUser trackUser : latestLeaderboardData) {
+            Ranking nextRanking = new Ranking(
+                    ++rank,
+                    trackUser.nickname,
+                    trackUser.location,
+                    getDistanceDisplay(trackUser.trackStatistics.getTotalDistance())
+            );
+            if (nextRanking.getScore().equals(lastScore)) {
+                consecutiveTies++;
+                nextRanking.setRank(rank - consecutiveTies);
+            }
+            else
+                consecutiveTies = 0;
+            rankingsData.add(nextRanking);
+            lastScore = nextRanking.getScore();
+        }
+        return rankingsData;
     }
 
-    private void sortRankingsByDistance(List<Ranking> rankings) {
-        rankings.sort((r1, r2) -> Double.compare(Double.parseDouble(r2.getScore()), Double.parseDouble(r1.getScore())));
+    private class SortByBestDistance implements Comparator<LeaderboardPagerAdapter.PlaceHolderTrackUser> {
+        @Override
+        public int compare(LeaderboardPagerAdapter.PlaceHolderTrackUser user1, LeaderboardPagerAdapter.PlaceHolderTrackUser user2) {
+            Distance user1Distance = user1.trackStatistics.getTotalDistance();
+            Distance user2Distance = user2.trackStatistics.getTotalDistance();
 
-        for (int i = 0; i < rankings.size(); i++) {
-            Ranking ranking = rankings.get(i);
-            ranking.setRank(i + 1);
+            if (user1Distance.lessThan(user2Distance))
+                return 1;
+            else if (user1Distance.greaterThan(user2Distance))
+                return -1;
+            return 0;
         }
     }
 
-    private List<Ranking> getTestData() {
-        List<Ranking> rankings = new ArrayList<>();
-        rankings.add(new Ranking(1, "User 1", "A",  Double.toString(20)));
-        rankings.add(new Ranking(2, "User 2", "B",  Double.toString(6)));
-        rankings.add(new Ranking(3, "User 3", "C",  Double.toString(23)));
-        rankings.add(new Ranking(4,  "User 4", "D",  Double.toString(22)));
-        rankings.add(new Ranking(5,  "User 5", "E",  Double.toString(21)));
-        rankings.add(new Ranking(6,  "User 6", "F",  Double.toString(30)));
-        rankings.add(new Ranking(7,  "User 7", "G",  Double.toString(19)));
-        rankings.add(new Ranking(8,  "User 8", "H",  Double.toString(32)));
-        rankings.add(new Ranking(9,  "User 9", "I",  Double.toString(17)));
-        rankings.add(new Ranking(10,  "User 10", "J",  Double.toString(16)));
-        rankings.add(new Ranking(11,  "User 11", "K",  Double.toString(15)));
-        rankings.add(new Ranking(12,  "User 12", "L",  Double.toString(14)));
-        rankings.add(new Ranking(13,  "User 13", "M",  Double.toString(28)));
-        rankings.add(new Ranking(14,  "User 14", "N",  Double.toString(12)));
-        rankings.add(new Ranking(15, "User 15", "O",  Double.toString(11)));
-        return rankings;
-    }
-
-
-    private List<Ranking> getAltTestData() {
-        List<Ranking> rankings = new ArrayList<>();
-        rankings.add(new Ranking(1, "First", "Steamboat Springs",  Double.toString(20)));
-        rankings.add(new Ranking(2, "Second", "North California CA",  Double.toString(24)));
-        rankings.add(new Ranking(3, "Third", "Steamboat Springs, Color Red",  Double.toString(28)));
-        rankings.add(new Ranking(4,  "Fourth", "Montreal",  Double.toString(22)));
-        rankings.add(new Ranking(5,  "Fifth", "Steamboat Springs",  Double.toString(21)));
-        rankings.add(new Ranking(6,  "Sixth", "Montreal",  Double.toString(30)));
-        rankings.add(new Ranking(7,  "Seven", "Steamboat Springs",  Double.toString(25)));
-        return rankings;
-    }
 }
