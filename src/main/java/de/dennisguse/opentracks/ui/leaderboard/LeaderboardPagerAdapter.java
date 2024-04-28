f5ce23af919366b40ce03ab9abeb25f91d3919a5
Palmarino DiMarco <palmardimarco@icloud.com>
2024-04-28 13:38:10 -0400

    Added a way to switch up the test data

--- src/main/java/de/dennisguse/opentracks/ui/leaderboard/LeaderboardPagerAdapter.java
+++ src/main/java/de/dennisguse/opentracks/ui/leaderboard/LeaderboardPagerAdapter.java
@@ -24,6 +24,7 @@ public class LeaderboardPagerAdapter extends FragmentPagerAdapter {
     private AverageMovingSpeedLeaderboardFragment averageMovingSpeedLeaderboardFragment;
     private LeaderboardFragment currentLeaderboardFragment;
     private LeaderboardFragment.LeaderboardType currentLeaderboardType;
+    private boolean altTestData;
 
     public LeaderboardPagerAdapter(FragmentManager fm) {
         super(fm);
@@ -102,7 +103,7 @@ public class LeaderboardPagerAdapter extends FragmentPagerAdapter {
             return LeaderboardPagerAdapter.LeaderboardType.MAX_SPEED.getTitle();
         else if (position == LeaderboardPagerAdapter.LeaderboardType.AVERAGE_MOVING_SPEED.value)
             return LeaderboardPagerAdapter.LeaderboardType.AVERAGE_MOVING_SPEED.getTitle();
-        return null;
+        return "";
     }
 
     public void setCurrentLeaderboardType(LeaderboardFragment.LeaderboardType leaderboardType) {
@@ -138,17 +139,19 @@ public class LeaderboardPagerAdapter extends FragmentPagerAdapter {
         stats.setMovingTime(Duration.ofMinutes(900));
         testData.add(new PlaceHolderTrackUser("User One", "Steamboat Springs", true, stats));
 
-        stats = new TrackStatistics();
-        stats.setTotalDistance(new Distance(550));
-        stats.setMaxSpeed(new Speed(50));
-        stats.setMovingTime(Duration.ofMinutes(950));
-        testData.add(new PlaceHolderTrackUser("User One","North California CA", true, stats));
+        if (altTestData) {
+            stats = new TrackStatistics();
+            stats.setTotalDistance(new Distance(550));
+            stats.setMaxSpeed(new Speed(50));
+            stats.setMovingTime(Duration.ofMinutes(950));
+            testData.add(new PlaceHolderTrackUser("User One","North California CA", true, stats));
 
-        stats = new TrackStatistics();
-        stats.setTotalDistance(new Distance(100));
-        stats.setMaxSpeed(new Speed(90));
-        stats.setMovingTime(Duration.ofHours(2));
-        testData.add(new PlaceHolderTrackUser("User Two", "Steamboat Springs", true, stats));
+            stats = new TrackStatistics();
+            stats.setTotalDistance(new Distance(100));
+            stats.setMaxSpeed(new Speed(90));
+            stats.setMovingTime(Duration.ofHours(2));
+            testData.add(new PlaceHolderTrackUser("User Two", "Steamboat Springs", true, stats));
+        }
 
         stats = new TrackStatistics();
         stats.setTotalDistance(new Distance(150));
@@ -156,11 +159,13 @@ public class LeaderboardPagerAdapter extends FragmentPagerAdapter {
         stats.setMovingTime(Duration.ofHours(1));
         testData.add(new PlaceHolderTrackUser("User Two", "Steamboat Springs", true, stats));
 
-        stats = new TrackStatistics();
-        stats.setTotalDistance(new Distance(1000));
-        stats.setMaxSpeed(new Speed(20));
-        stats.setMovingTime(Duration.ofHours(3));
-        testData.add(new PlaceHolderTrackUser("User Three","Steamboat Springs", true, stats));
+        if (altTestData) {
+            stats = new TrackStatistics();
+            stats.setTotalDistance(new Distance(1000));
+            stats.setMaxSpeed(new Speed(20));
+            stats.setMovingTime(Duration.ofHours(3));
+            testData.add(new PlaceHolderTrackUser("User Three","Steamboat Springs", true, stats));
+        }
 
         stats = new TrackStatistics();
         stats.setTotalDistance(new Distance(900));
@@ -174,11 +179,13 @@ public class LeaderboardPagerAdapter extends FragmentPagerAdapter {
         stats.setMovingTime(Duration.ofHours(6));
         testData.add(new PlaceHolderTrackUser("User Four","Steamboat Springs", true, stats));
 
-        stats = new TrackStatistics();
-        stats.setTotalDistance(new Distance(2500));
-        stats.setMaxSpeed(new Speed(55));
-        stats.setMovingTime(Duration.ofHours(6));
-        testData.add(new PlaceHolderTrackUser("User Four","North California CA", true, stats));
+        if (altTestData) {
+            stats = new TrackStatistics();
+            stats.setTotalDistance(new Distance(2500));
+            stats.setMaxSpeed(new Speed(55));
+            stats.setMovingTime(Duration.ofHours(6));
+            testData.add(new PlaceHolderTrackUser("User Four","North California CA", true, stats));
+        }
 
         stats = new TrackStatistics();
         stats.setTotalDistance(new Distance(10000));
@@ -186,11 +193,13 @@ public class LeaderboardPagerAdapter extends FragmentPagerAdapter {
         stats.setMovingTime(Duration.ofHours(4));
         testData.add(new PlaceHolderTrackUser("User Five","North California CA", true, stats));
 
-        stats = new TrackStatistics();
-        stats.setTotalDistance(new Distance(1000));
-        stats.setMaxSpeed(new Speed(50));
-        stats.setMovingTime(Duration.ofHours(1));
-        testData.add(new PlaceHolderTrackUser("User Five","North California CA", true, stats));
+        if (altTestData) {
+            stats = new TrackStatistics();
+            stats.setTotalDistance(new Distance(1000));
+            stats.setMaxSpeed(new Speed(50));
+            stats.setMovingTime(Duration.ofHours(1));
+            testData.add(new PlaceHolderTrackUser("User Five","North California CA", true, stats));
+        }
 
         stats = new TrackStatistics();
         stats.setTotalDistance(new Distance(1000));
@@ -216,29 +225,33 @@ public class LeaderboardPagerAdapter extends FragmentPagerAdapter {
         stats.setMovingTime(Duration.ofMinutes(30));
         testData.add(new PlaceHolderTrackUser("User Nine", "North California CA", true, stats));
 
-        stats = new TrackStatistics();
-        stats.setTotalDistance(new Distance(154));
-        stats.setMaxSpeed(new Speed(150));
-        stats.setMovingTime(Duration.ofMinutes(59));
-        testData.add(new PlaceHolderTrackUser("User Ten","Steamboat Springs", true, stats));
+        if (altTestData) {
+            stats = new TrackStatistics();
+            stats.setTotalDistance(new Distance(154));
+            stats.setMaxSpeed(new Speed(150));
+            stats.setMovingTime(Duration.ofMinutes(59));
+            testData.add(new PlaceHolderTrackUser("User Ten","Steamboat Springs", true, stats));
 
-        stats = new TrackStatistics();
-        stats.setTotalDistance(new Distance(134));
-        stats.setMaxSpeed(new Speed(130));
-        stats.setMovingTime(Duration.ofMinutes(59));
-        testData.add(new PlaceHolderTrackUser("User Eleven","Steamboat Springs", true, stats));
+            stats = new TrackStatistics();
+            stats.setTotalDistance(new Distance(134));
+            stats.setMaxSpeed(new Speed(130));
+            stats.setMovingTime(Duration.ofMinutes(59));
+            testData.add(new PlaceHolderTrackUser("User Eleven","Steamboat Springs", true, stats));
 
-        stats = new TrackStatistics();
-        stats.setTotalDistance(new Distance(159));
-        stats.setMaxSpeed(new Speed(120));
-        stats.setMovingTime(Duration.ofMinutes(34));
-        testData.add(new PlaceHolderTrackUser("User Twelve","Steamboat Springs", true, stats));
+            stats = new TrackStatistics();
+            stats.setTotalDistance(new Distance(159));
+            stats.setMaxSpeed(new Speed(120));
+            stats.setMovingTime(Duration.ofMinutes(34));
+            testData.add(new PlaceHolderTrackUser("User Twelve","Steamboat Springs", true, stats));
+        }
 
         stats = new TrackStatistics();
         stats.setTotalDistance(new Distance(100));
         stats.setMaxSpeed(new Speed(90));
         stats.setMovingTime(Duration.ofHours(2));
         testData.add(new PlaceHolderTrackUser("EXCLUDE ME!","North California CA", false, stats));
+
+        altTestData = !altTestData;
         return testData;
     }
 
